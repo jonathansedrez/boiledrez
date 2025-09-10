690883bcfcd9dc0a9234aa0647485150790dbc8b
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-09-09 21:36:29 -0300

    feat: add RTL

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { expect, test } from "vitest";
 import App from "./App";
 
 test("should render App", () => {
   expect(App).toBeDefined();
+});
+
+test("should increment counter if click on Add button", async () => {
+  render(<App />);
+  const button = screen.getByText("Add");
+  expect(screen.getByText(/Count: 0/)).toBeInTheDocument();
+  await userEvent.click(button);
+  expect(screen.getByText(/Count: 1/)).toBeInTheDocument();
 });
